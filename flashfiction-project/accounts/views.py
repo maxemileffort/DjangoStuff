cfbd8371fb616588fa964003178f0be514fff365
Max <max.w29@hotmail.com>
2018-06-03 09:25:52 -0500

    added authentication

--- flashfiction-project/accounts/views.py
+++ flashfiction-project/accounts/views.py
@@ -1,15 +1,38 @@
-from django.shortcuts import render
-
+from django.shortcuts import render, redirect
+from django.contrib.auth.models import User
+from django.contrib import auth
 # Create your views here.
 
 
 def register(request):
-    return render(request, 'register.html')
+    if request.method == 'POST':
+        if request.POST['password1'] == request.POST['password2']:
+            try:
+                user = User.objects.get(username=request.POST['username'])
+                return render(request, 'register.html', {'error': 'Username already exists.'})
+            except User.DoesNotExist:
+                user = User.objects.create_user(request.POST['username'], password=request.POST['password1'])
+                auth.login(request, user)
+                return redirect('dashboard')
+        else:
+            return render(request, 'register.html', {'error': 'Passwords must match.'})
+    else:
+        return render(request, 'register.html')
 
 
 def login(request):
-    return render(request, 'login.html')
+    if request.method == 'POST':
+        user = auth.authenticate(username=request.POST['username'], password=request.POST['password'])
+        if user is not None:
+            auth.login(request, user)
+            return redirect('dashboard')
+        else: 
+            return render(request, 'login.html', {'error': 'Username or password is incorrect'})
+    else:
+        return render(request, 'login.html')
 
 
 def logout(request):
-    return render(request, 'homeinfo.html')
+    if request.method == 'POST':
+        auth.logout(request)
+        return redirect('homeinfo')
